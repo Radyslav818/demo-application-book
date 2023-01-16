24cfe19035bcf873a46235c0aca051ef49dfe09b
Vladyslav Radetskyi <109795929+Radyslav818@users.noreply.github.com>
2023-01-16 22:22:36 +0200

    add useParams

--- src/componets/BookDetails.jsx
+++ src/componets/BookDetails.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { BOOK_DETAILS_URL } from "../API";
 
 function BookDetails() {
-  return <div>BookDetails</div>;
+  const { id } = useParams();
+  const [book, setBook] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`${BOOK_DETAILS_URL}/${id}`)
+      .then((res) => {
+        setBook(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
+
+  return (
+    <div>
+      <div>
+        <h2>{book?.title}</h2>
+      </div>
+      <div></div>
+    </div>
+  );
 }
 
 export default BookDetails;
--- src/componets/BookList.jsx
+++ src/componets/BookList.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { API_URL } from "../API";
 import axios from "axios";
+import { useNavigate } from "react-router";
 
 const BookList = () => {
   const [books, setBooks] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios
@@ -15,22 +17,28 @@ const BookList = () => {
       .catch((err) => console.log(err));
   }, []);
   return (
-    <div className="min-h-[80vh] w-full grid grid-cols-3 justify-items-center">
-      {books.map((book) => {
-        return (
-          <div key={book.id}>
-            <div>
-              <h2>{book.title}</h2>
-            </div>
-            <div>
-              <img src={book.image_url} alt="#" />
-            </div>
-            <div>
-              <button>Add to Favorites</button>
-            </div>
+    <div className="min-h-[80vh] w-full grid grid-cols-[repeat(3,1fr)] justify-items-center text-center">
+      {books.map((book) => (
+        <div
+          key={book.id}
+          className="text-center flex flex-col align-center justify-center mx-5 my-0"
+        >
+          <div>
+            <h2>{book.title}</h2>
           </div>
-        );
-      })}
+          <div>
+            <img
+              className=""
+              src={book.image_url}
+              alt="#"
+              onClick={() => navigate(`/books/${book.id}`)}
+            />
+          </div>
+          <div>
+            <button>Add to Favorites</button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
